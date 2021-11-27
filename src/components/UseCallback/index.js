ee17e73f3f3570773b75a11f5548e805b0449eff
wenbang.he@okcoin.net <wenbang.he@okg.com>
2021-11-27 22:21:36 +0800

    useCallback

--- src/components/UseCallback/index.js
+++ src/components/UseCallback/index.js
@@ -2,38 +2,50 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
 var fn = null;
 export default function  Callback() {
-  const [count, setCount] = useState(0);
-  const [num, setNum] = useState(0);
+ const { count, num, action} = useAdd();
 //  var obj = {};
  var callback = ()=>{}
 //  var newObj = useMemo(() => {
 //      return <div>useMemo</div>
 //  }, [])
  const newCallback = useCallback(callback, [count])
-console.log('callback equal fn', Object.is(newCallback, fn))
- fn = newCallback;
+// console.log('callback equal fn', Object.is(newCallback, fn))
+//  fn = newCallback;
   useEffect(() => {
     console.log('useEffect1')
     document.title = `You clicked ${count} times`;
   }, []);
-  const changeCount = useCallback(
-    () => setCount(count + 1),
-    []
-  )
-  const changeNum = useCallback(
-    () => setNum(num + 1),
-    []
-  )
+
+  console.log('callback equal fn', Object.is(action, fn))
+  
+  fn = action;
+  
   return (
     <div>
         {console.log('render')}
       <p>You clicked {count} times</p>
-      <button onClick={changeCount}>
-        Click me count
+      <button onClick={action.changeCount}>
+        Click me count {count}
       </button>
-      <button onClick={changeNum}>
-        Click me num
+      <button onClick={action.changeNum}>
+        Click me num ${num}
       </button>
     </div>
   );
+}
+
+function useAdd () {
+    const [count, setCount] = useState(0);
+    const [num, setNum] = useState(0);
+    const action  = useMemo(()=>{
+        return  {
+            changeCount: () =>setCount((s)=>s+1),
+            changeNum: () => setNum((s)=>s+1)
+        }
+      }, []);
+      return  {
+        count,
+        num,
+        action
+      }
 }
